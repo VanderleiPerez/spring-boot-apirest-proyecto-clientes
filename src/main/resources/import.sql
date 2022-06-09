e9ba4497a71e076609c7fd68112b0dea417512ec
VanderleiPerez <1624655@utp.edu.pe>
2022-06-09 02:18:17 -0500

    actualización upload

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,131 @@ INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pasc
 INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
 INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
 INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
+
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Vanderlei','Pérez','correo1@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Rogger','Camarena','correo2@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Hiro','Hamada','correo3@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo4@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo5@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Ruggero','Pascuarelli','correo6@gmail.com','2022-05-22');
+INSERT INTO clientes(nombre, apellido, email, create_at) VALUES ('Simbar','Dalas','correo7@gmail.com','2022-05-22');
